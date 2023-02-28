4482b0914a97b82965173d1d43d2b0f084557977
mattmpe09 <mattmpe09@gmail.com>
2023-02-28 15:34:23 +0000

    conflict is resolved

--- app.java
+++ app.java
@@ -2,3 +2,4 @@ This devOps training in getting more interesting by the day
 We want to use java to develop am application for a FinTech company. I want very member the team to be very active. We will continue this work
 
 .net application
+more changes are coming
